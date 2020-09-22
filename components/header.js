3a3c08dfc9b6e53f1211de6d6f8dcfb7fcffdd78
Jacob <jacobt1206@gmail.com>
2020-09-21 22:24:06 -0500

    truly finished navbar lmao!

--- components/header.js
+++ components/header.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import useSWR from "swr";
-import styles from "./header.module.css";
 
 const Header = () => {
   const router = useRouter();
@@ -19,7 +18,7 @@ const Header = () => {
   };
 
   return (
-    <div className={styles.header}>
+    <div className="div">
       <header>
         <nav>
           <ul>
@@ -36,21 +35,79 @@ const Header = () => {
               </>
             ) : (
               <>
-                <li>
-                  <Link href="/login">
-                    <a>Login</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/signup">
-                    <a>Signup</a>
-                  </Link>
-                </li>
+                <div className="row">
+                  <div className="logo">
+                    <Link href="/">
+                      <a>Schedyall</a>
+                    </Link>
+                  </div>
+                  <div className="btns">
+                    <Link href="/login">
+                      <a>
+                        <b>Login</b>
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="btn">
+                    <Link href="/signup">
+                      <a>
+                        <b>Sign Up</b>
+                      </a>
+                    </Link>
+                  </div>
+                </div>
               </>
             )}
           </ul>
         </nav>
       </header>
+      <style jsx>
+        {`
+          .div {
+            background-color: none;
+            position: fixed;
+            width: 100%;
+          }
+          a {
+            color: black;
+            font-size: 125%;
+          }
+          .logo {
+            float: left;
+            margin-left: 0%;
+          }
+          .btns {
+            border: 1px solid #232b2b;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+            padding: 1rem;
+            float: right;
+            margin-right: 3%;
+          }
+          .btn {
+            border: 1px solid #232b2b;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+            padding: 1rem;
+            float: right;
+            margin-right: 2%;
+            color: inherit;
+            text-decoration: none;
+          }
+          .btn:hover,
+          .btn:focus,
+          .btn:active {
+            color: #eaeaea;
+            border-color: #eaeaea;
+          }
+          .btns:hover,
+          .btns:focus,
+          .btns:active {
+            color: #eaeaea;
+            border-color: #eaeaea;
+          }
+        `}
+      </style>
     </div>
   );
 };
