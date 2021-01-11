dc7882beedbe8464e21384429ea46c2aa49b0c10
Jacob <jacobt1206@gmail.com>
2021-01-10 23:23:19 -0600

    SEO fix

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Header from "../components/header";
 import styles from "./layout.module.css";
+import { NextSeo } from 'next-seo';
 
 const Layout = ({ children }) => (
   <>
+    <NextSeo
+      title="Schedyall"
+      description="A better way to plan your day!"
+    />
     <Head>
       <title>Schedyall</title>
       <link rel="icon" href="/favicon.JPG" />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 //Have I not commanded you? Be strong and courageous. Do not be afraid; do not be discouraged, for the LORD your God will be with you wherever you go." - Joshua 1:9
-
+import Head from 'next/head'
 import Link from "next/link";
 import useSWR from "swr";
 import { gql } from "graphql-request";
